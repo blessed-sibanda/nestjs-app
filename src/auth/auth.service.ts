1e16912756c3668d528096b22da40d4c27b43e45
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-01-10 08:12:02 +0200

    generate jwt for user after signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { IUser, UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, password: string): Promise<any> {
     const user = await this.usersService.findOne(username);
@@ -12,5 +16,12 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: IUser) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
