39c2e9bfa2d4514e8dea3791a7e362d102251564
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-01-10 08:35:02 +0200

    use email & password for local signin instead

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,27 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
 export interface IUser {
-  userId: number;
-  username: string;
+  id: number;
+  name: string;
   password: string;
+  email: string;
 }
 
 @Injectable()
 export class UsersService {
   private readonly users: IUser[] = [
     {
-      userId: 1,
-      username: 'Blessed',
+      id: 1,
+      name: 'Blessed Sibanda',
       password: '1234pass',
+      email: 'blessed@example.com',
     },
     {
-      userId: 2,
-      username: 'Thelma',
+      id: 2,
+      name: 'Thelma Toro Sibanda',
       password: 'guess',
+      email: 'thelma@example.com',
     },
   ];
 
-  async findOne(username: string): Promise<IUser | undefined> {
-    return this.users.find((user) => user.username === username);
+  async findOne(email: string): Promise<IUser | undefined> {
+    return this.users.find((user) => user.email === email);
   }
 }
