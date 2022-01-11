a842d201124fdb127c39f716623e0fd74639f11c
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-01-11 17:18:38 +0200

    auth controller specs

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,22 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from '../users/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
-  let authServ: AuthService;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [AuthService],
     })
       .overrideProvider(AuthService)
       .useValue({ login: () => null })
       .compile();
 
-    authServ = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
     controller = module.get<AuthController>(AuthController);
   });
 
-  describe('login', () => {});
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should login', () => {
+      let login = jest.spyOn(authService, 'login');
+      let req = { user: new User({ email: 'test@example.com' }) };
+      controller.login(req);
+      expect(login).toBeCalledWith(req.user);
+    });
+  });
 });
--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -1,71 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { IPaginationOptions } from 'nestjs-typeorm-paginate';
-import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-
-const ble = new User({
-  name: 'Blessed',
-  email: 'blessed@example.com',
-  password: '1234pass',
-});
-
-const thelma = new User({
-  name: 'Thelma',
-  email: 'thelma@example.com',
-  password: '1234pass',
-});
-
-const mike = new User({
-  name: 'Mike',
-  email: 'mike@example.com',
-  password: '1234pass',
-});
-
-const userArray: User[] = [ble, thelma, mike];
-
-const mockFindAll = (options: IPaginationOptions, q?: string) => {
-  let limit = options.limit as number;
-  let totalPages = Math.ceil(userArray.length / limit);
-  let items: User[];
-  if (q) {
-    items = userArray.filter((i) => {
-      q = q.toLowerCase();
-      return (
-        i.name.toLowerCase().startsWith(q) ||
-        i.email.toLowerCase().startsWith(q)
-      );
-    });
-  } else items = userArray;
-
-  let resultItems = items.slice(0, limit);
-  let currentPage = options.page as number;
-
-  let result = {
-    items: resultItems,
-    meta: {
-      totalItems: userArray.length,
-      itemCount: resultItems.length,
-      itemsPerPage: limit,
-      totalPages,
-      currentPage: options.page,
-    },
-    links: {
-      first: `${options.route}?limit=${limit}`,
-      previous:
-        options.page > 1
-          ? `${options.route}?page=${currentPage - 1}&limit=${limit}`
-          : '',
-      next:
-        currentPage < totalPages
-          ? `${options.route}?page=${currentPage + 1}&limit=${limit}`
-          : '',
-      last: `${options.route}?page=${currentPage}&limit=${limit}`,
-    },
-  };
-
-  return result;
-};
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -77,9 +12,7 @@ describe('UsersController', () => {
       providers: [UsersService],
     })
       .overrideProvider(UsersService)
-      .useValue({
-        findAll: mockFindAll,
-      })
+      .useValue({})
       .compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -88,12 +21,5 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
-  });
-
-  describe('index', () => {
-    it('should return paginated user list', () => {
-      let result = controller.index(1, 10, '');
-      expect(result['items']).toEqual(userArray);
-    });
   });
 });
